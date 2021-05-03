212e91de20d42d0c854acab11fdc0b27b0de8885
Nakai <nakai.hamlin18@gmail.com>
2021-05-03 18:45:00 -0400

    commit tree 2fae7287f81dc5481dd9947002e3bcdd5df2ce22

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,19 +2,17 @@ class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
   end
-
   def new
   end
 
   def create
-    @author = Author.create!(author_params)
-
-    redirect_to author_path(@author)
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
 
   private
-
-  def author_params
-    params.permit(:email, :name)
-  end
-end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,25 @@
 class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
-
   def show
   end
-
   def edit
   end
 
   def update
+    #binding.pry
     @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
-
   def post_params
     params.permit(:category, :content, :title)
   end
-
   def set_post!
     @post = Post.find(params[:id])
   end
